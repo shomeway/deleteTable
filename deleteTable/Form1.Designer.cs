aa23f5ede1bd4868e2ede792e03f8b650b46cada
shomeway <daniel0713520@hotmail.com>
2017-04-07 16:49:38 +0800

    First
    Useless Table Delete Tool

---
+++ deleteTable/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace deleteTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox_IP = new System.Windows.Forms.TextBox();
+            this.textBox_Log = new System.Windows.Forms.TextBox();
+            this.button_Ask = new System.Windows.Forms.Button();
+            this.button_Drop = new System.Windows.Forms.Button();
+            this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.textBox_From = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox_IP
+            // 
+            this.textBox_IP.Location = new System.Drawing.Point(59, 12);
+            this.textBox_IP.Name = "textBox_IP";
+            this.textBox_IP.Size = new System.Drawing.Size(129, 22);
+            this.textBox_IP.TabIndex = 0;
+            // 
+            // textBox_Log
+            // 
+            this.textBox_Log.BackColor = System.Drawing.Color.White;
+            this.textBox_Log.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_Log.Location = new System.Drawing.Point(12, 71);
+            this.textBox_Log.Multiline = true;
+            this.textBox_Log.Name = "textBox_Log";
+            this.textBox_Log.ReadOnly = true;
+            this.textBox_Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Log.Size = new System.Drawing.Size(261, 226);
+            this.textBox_Log.TabIndex = 3;
+            // 
+            // button_Ask
+            // 
+            this.button_Ask.Location = new System.Drawing.Point(194, 10);
+            this.button_Ask.Name = "button_Ask";
+            this.button_Ask.Size = new System.Drawing.Size(55, 22);
+            this.button_Ask.TabIndex = 2;
+            this.button_Ask.Text = "Ask";
+            this.button_Ask.UseVisualStyleBackColor = true;
+            this.button_Ask.Click += new System.EventHandler(this.button_Ask_Click);
+            // 
+            // button_Drop
+            // 
+            this.button_Drop.Location = new System.Drawing.Point(194, 38);
+            this.button_Drop.Name = "button_Drop";
+            this.button_Drop.Size = new System.Drawing.Size(55, 22);
+            this.button_Drop.TabIndex = 3;
+            this.button_Drop.Text = "Drop";
+            this.button_Drop.UseVisualStyleBackColor = true;
+            this.button_Drop.Click += new System.EventHandler(this.button_Drop_Click);
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Location = new System.Drawing.Point(45, 40);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(45, 22);
+            this.textBox_Port.TabIndex = 1;
+            // 
+            // textBox_From
+            // 
+            this.textBox_From.Location = new System.Drawing.Point(135, 40);
+            this.textBox_From.Name = "textBox_From";
+            this.textBox_From.Size = new System.Drawing.Size(53, 22);
+            this.textBox_From.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Table IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(96, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "From:";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 302);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(243, 12);
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.MouseHover += new System.EventHandler(this.progressBar1_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 320);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_From);
+            this.Controls.Add(this.textBox_Port);
+            this.Controls.Add(this.button_Drop);
+            this.Controls.Add(this.button_Ask);
+            this.Controls.Add(this.textBox_Log);
+            this.Controls.Add(this.textBox_IP);
+            this.Name = "Form1";
+            this.Text = "Useless Table Dropping Tool (version:X)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_IP;
+        private System.Windows.Forms.TextBox textBox_Log;
+        private System.Windows.Forms.Button button_Ask;
+        private System.Windows.Forms.Button button_Drop;
+        private System.Windows.Forms.TextBox textBox_Port;
+        private System.Windows.Forms.TextBox textBox_From;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ deleteTable/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using MySql.Data.MySqlClient;
+
+namespace deleteTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bgwWorker.DoWork += new DoWorkEventHandler(bgwWorker_DoWork);
+            bgwWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bgwWorker_RunWorkerCompleted);
+            bgwWorker.ProgressChanged += new ProgressChangedEventHandler(bgwWorker_ProgressChanged);
+            bgwWorker.WorkerReportsProgress = true;
+            bgwWorker.WorkerSupportsCancellation = true;
+        }
+
+        private bool flag = true;
+        private MySqlConnection mConn;
+        private MySqlCommand mCommand;
+        private MySqlDataReader mReader;
+        private List<string> result_List;
+        BackgroundWorker bgwWorker = new BackgroundWorker();
+
+        private void button_Ask_Click(object sender, EventArgs e)
+        {
+            StringBuilder sqlBuilder = new StringBuilder();
+            //string sql = "SELECT CONCAT( 'DROP TABLE ', GROUP_CONCAT(table_name) , ';' ) AS statement FROM information_schema.tables WHERE table_schema = 'netflow' AND table_name LIKE '{0}'";
+            string sql = "SHOW TABLES FROM netflow LIKE '{0}'";
+            try
+            {
+                int x = Convert.ToInt32(textBox_From.Text);
+                this.textBox_Log.Text = "Start Connection";
+                Connection();
+                result_List = new List<string>();
+                //combine query string
+                sqlBuilder = new StringBuilder();
+                string query = this.textBox_IP.Text.ToString().Replace('.', '_');
+                query += "_" + x.ToString() + "%";
+                sqlBuilder = sqlBuilder.AppendFormat(sql, query);
+                //execute query
+                mCommand = new MySqlCommand(sqlBuilder.ToString() + ";", mConn);
+                mCommand.CommandTimeout = 120;
+                mReader = mCommand.ExecuteReader();
+                this.textBox_Log.AppendText("\r\nStart show table : " + query);
+
+                while (mReader.Read())
+                {
+                    result_List.Add(mReader.GetString(0));
+                }
+
+                foreach (string s in result_List)
+                {
+                    this.textBox_Log.AppendText("\r\n" + s);
+                }
+                CloseConnection();
+                Thread.Sleep(1000);
+                this.textBox_Log.Text += "\r\nDone";
+            }
+            catch (Exception ex)
+            {
+                this.textBox_Log.Text += ex.Message;
+            }
+        }
+
+        private void button_Drop_Click(object sender, EventArgs e)
+        {
+            if (flag)
+            {
+                this.button_Drop.Text = "Stop";
+                flag = false;
+                this.textBox_Log.Text = "Start!";
+                bgwWorker.RunWorkerAsync();
+                this.textBox_Log.AppendText("\r\nComputing...");
+            }
+            else
+            {
+                this.button_Drop.Text = "Drop";
+                bgwWorker.CancelAsync();
+                this.textBox_Log.AppendText("\r\nCanceled!");
+                flag = true;
+            }
+        }
+
+        public void Connection()
+        {
+            try
+            {
+                //configs
+                string ConnStr = "Database=;Server=127.0.0.1;UserID=;Password=;Port="+textBox_Port.Text.ToString()+";CHARSET=utf8";
+                mConn = new MySqlConnection(ConnStr);
+                mConn.Open();
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (mConn != null)
+                this.mConn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int count = Convert.ToInt32(this.textBox_Port.Text);
+
+            for (int i = 0; i < count; i++)
+            {
+                button_Ask.PerformClick();
+                Thread.Sleep(3000);
+                button_Drop.PerformClick();
+            }
+        }
+
+        private void bgwWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            StringBuilder sqlBuilder = new StringBuilder();
+            string sql = "SHOW TABLES FROM netflow LIKE '{0}'";
+            string sql2 = "DROP TABLE {0};";
+            float current_progress = 0;
+            try
+            {
+                int start_count = Convert.ToInt32(textBox_From.Text);
+                for (int i = start_count; i < 1000; i++)
+                {
+                    if (bgwWorker.CancellationPending)
+                    {
+                        break;//stop thread
+                    }
+                    //int opreation always return an int, must convert one of them to float to get a float return
+                    current_progress = (i - start_count) / (float)(1000 - start_count) * 100;
+                    Connection();//連線
+                    result_List = new List<string>();
+                    sqlBuilder = new StringBuilder();
+                    //combine query string
+                    string query = this.textBox_IP.Text.ToString().Replace('.', '_');
+                    query += "_" + i.ToString() + "%";
+                    sqlBuilder = sqlBuilder.AppendFormat(sql, query);
+                    //exceute query
+                    mCommand = new MySqlCommand(sqlBuilder.ToString() + ";", mConn);
+                    mCommand.CommandTimeout = 180;//wait timeout
+                    mReader = mCommand.ExecuteReader();
+
+                    bgwWorker.ReportProgress((int)current_progress,"Start show table : "+query);
+                    string result = "";
+
+                    while (mReader.Read())
+                    {
+                        result = mReader.GetString(0);
+                        result_List.Add(mReader.GetString(0));
+                    }
+                    CloseConnection();
+                    Thread.Sleep(1000);
+
+                    //DROP===================================================
+                    bgwWorker.ReportProgress((int)current_progress,"Start dropping @ " + query);
+                    Connection();
+                    sqlBuilder = new StringBuilder();
+                    int return_value = 0;
+                    query = "";
+                    int count = result_List.Count();
+                    if (count > 0)
+                    {
+                        foreach (string s in result_List)
+                        {
+                            if (count > 1)
+                                query += s + ',';
+                            else
+                                query += s;
+                            count--;
+                        }
+
+                        sqlBuilder = sqlBuilder.AppendFormat(sql2, query);
+                        mCommand = new MySqlCommand(sqlBuilder.ToString(), mConn);
+                        mCommand.CommandTimeout = 120;
+                        return_value = mCommand.ExecuteNonQuery();
+                    }
+                    CloseConnection();
+                    bgwWorker.ReportProgress((int)current_progress,"Drop done @ " + query);
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                bgwWorker.ReportProgress((int)current_progress,ex.Message);
+            }
+        }
+
+        private void bgwWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+            
+            this.textBox_Log.AppendText("\r\n"+e.UserState.ToString());
+        }
+
+        private void bgwWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.textBox_Log.AppendText("\r\nDone");
+            if (flag == false)
+            {
+                this.button_Drop.Text = "Drop";
+                flag = true;
+            }
+        }
+
+        private void progressBar1_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show(this.progressBar1.Value.ToString(),progressBar1);
+        }
+    }
+}
